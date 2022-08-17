9bfd9348b4904e96921ded524d9c31101d29a39d
buluttuncel <tuncelbu@gmail.com>
2022-08-17 18:58:49 +0100

    tried to perfect my route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants do
+    member
+    resources :reviews
   end
 end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
